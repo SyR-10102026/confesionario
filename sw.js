c15d9cfdb71b651612ce8f4c41552a574ed8b89e
SyR-10102026 <bodasarayramon2026@gmail.com>
2026-09-10 22:27:38 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='confesionario-sr-v3';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./img/1.png','./img/2.png','./img/3.png','./img/4.png','./img/5.png','./img/6.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./index.html'))))});
